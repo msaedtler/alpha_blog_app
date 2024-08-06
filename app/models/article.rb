aadbfd9b5f88ae0763d2782c8fd2ddfaef68a009
Michael Saedtler <michael.saedtler@clickworker.com>
2024-08-06 11:48:39 +0200

    changes in the routes file

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 6, maximunm: 100}
+  validates :description, presence: true, length: {minimum: 10, maximunm: 300}
 end
