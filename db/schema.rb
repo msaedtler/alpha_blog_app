3da6e6a927d44d6e0d101edaf73db67c0d3ca06b
Michael Saedtler <michael.saedtler@clickworker.com>
2024-08-06 09:58:46 +0200

    add db column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_05_153051) do
+ActiveRecord::Schema.define(version: 2024_08_06_075418) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
