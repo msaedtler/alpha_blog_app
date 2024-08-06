fb2253c751318056390b1059049db971152f3b5a
Michael Saedtler <michael.saedtler@clickworker.com>
2024-08-06 15:44:51 +0200

    changed wrong wordings

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articales = Article.all
+    @articles = Article.all
   end
 #  before_action :set_article, only: %i[ show edit update destroy ]
 #
